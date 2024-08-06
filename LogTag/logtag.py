6edb153f3b85fb1a6153bf2aedb59925d2261aac
ShotaIuchi <shotaiuchi.develop@gmail.com>
2024-08-06 22:59:10 +0900

    Add config handling and output file support

--- LogTag/logtag.py
+++ LogTag/logtag.py
@@ -2,19 +2,47 @@ import argparse
 import json
 import os
 import sys
+from tempfile import TemporaryFile
+
+DOTDIR = '.logtag'
+
+PWD = os.getcwd()
+CWD = os.path.dirname(os.path.abspath(__file__))
+HOME = os.path.expanduser('~')
 
 
 def all_file_join(file_list: list) -> list:
-    all_file = []
+    all_line = []
+
     for file in file_list:
         with open(file, 'r') as f:
-            all_file += [line.rstrip() for line in f]
-    return all_file
+            lines = f.readlines()
+            lines = [line.rstrip() for line in lines]
+            all_line += lines
+
+    return all_line
 
 
-def load_json(file: str) -> dict:
+def read_dotfile(arg_directory: str, file: str) -> dict:
+    if arg_directory is None:
+        arg_directory = ''
+
+    # Priority: arg_directory > HOME > CWD > PWD
+    target_file = ''
+    if os.path.isfile(os.path.join(arg_directory, file)):
+        target_file = os.path.join(arg_directory, file)
+    elif os.path.isfile(os.path.join(HOME, DOTDIR, file)):
+        target_file = os.path.join(HOME, DOTDIR, file)
+    elif os.path.isfile(os.path.join(CWD, DOTDIR, file)):
+        target_file = os.path.join(CWD, DOTDIR, file)
+    elif os.path.isfile(os.path.join(PWD, DOTDIR, file)):
+        target_file = os.path.join(PWD, DOTDIR, file)
+    else:
+        print(f'Error: File not found: {file}')
+        sys.exit(1)
+
     try:
-        with open(file, 'r') as f:
+        with open(target_file, 'r') as f:
             return json.load(f)
     except FileNotFoundError:
         print(f'Error: File not found: {file}')
@@ -27,45 +55,72 @@ def load_json(file: str) -> dict:
 def main():
     parser = argparse.ArgumentParser(description='LogTag adds tags to log messages.')
     parser.add_argument('-f', '--file', type=str, nargs='+', help='Files to add tags.')
+    parser.add_argument('-o', '--out', type=str, help='Output file.')
     parser.add_argument('-s', '--sort', action='store_true', help='Sort log messages.')
-    parser.add_argument('-o', '--only', action='store_true', help='Display only tagged log messages.')
+    parser.add_argument('-u', '--uniq', action='store_true', help='Remove duplicate log messages.')
+    parser.add_argument('--config', type=str, help='Config file.')
 
     args = parser.parse_args()
 
+    # read config files
+    cfg = read_dotfile(args.config, 'config.json')
+    tag = read_dotfile(args.config, 'logtag.tag.json')
+    filter = read_dotfile(args.config, 'logtag.filter.json')
+
+    space = cfg['space']
+    filter_display = filter['display']
+
+    # read all log messages
     if args.file:
         all_file = all_file_join(args.file)
 
+        # sort log messages
         if args.sort:
             all_file = sorted(all_file)
+    else:
+        print('Error: No input files.')
+        sys.exit(1)
 
-    cwd = os.getcwd()
-    pwd = os.path.dirname(os.path.abspath(__file__))
+    # convert log messages
+    with TemporaryFile('w+') as tp:
+        # lambda function, write to tempolary file
+        def print_tp(msg, line): return tp.write(f'{msg:<{space}}{line}\n')
 
-    cfg = load_json(os.path.join(cwd, '.logtag', 'config.json'))
-    tag = load_json(os.path.join(cwd, '.logtag', 'logtag.tag.json'))
-    filter = load_json(os.path.join(cwd, '.logtag', 'logtag.filter.json'))
-
-    space = cfg['space']
-
-    for line in all_file:
-        msg = ''
-        for key, value in tag.items():
-            if key in line:
-                msg = value
-                if args.only:
-                    print(f'{msg:<{space}}{line}')
-                break
-
-        if args.only:
-            continue
-
-        if len(filter['display']) > 0:
-            for key in filter['display']:
+        # convert all log messages
+        for line in all_file:
+            msg = ''
+            for key, value in tag.items():
                 if key in line:
-                    print(f'{msg:<{space}}{line}')
+                    msg = value
+                    if args.uniq:
+                        print_tp(msg, line)
                     break
-        else:
-            print(f'{msg:<{space}}{line}')
+
+            # remove duplicate log messages
+            if args.uniq:
+                continue
+
+            # filter log messages
+            if len(filter_display) > 0:
+                # filter log messages
+                for key in filter_display:
+                    if key in line:
+                        print_tp(msg, line)
+                        break
+            else:
+                print_tp(msg, line)
+
+        # print converted log messages
+        tp.seek(0)
+        for line in tp.readlines():
+            print(line.rstrip())
+
+        # write to output file
+        tp.seek(0)
+        if args.out:
+            with open(args.out, 'w') as f:
+                for line in tp.readlines():
+                    f.write(line)
 
 
 if __name__ == '__main__':
